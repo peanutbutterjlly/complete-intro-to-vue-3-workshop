3736745c9ff116fbaa687e93b8ced65649edd0dc
steve <steverios_sr@yahoo.com>
2023-07-11 20:08:24 -0500

    pinia

--- cest-la-vue/src/main.js
+++ cest-la-vue/src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import { createRouter, createWebHashHistory } from "vue-router";
 import { routes } from "./router";
 import App from "./App.vue";
 
 const app = createApp(App);
+const pinia = createPinia();
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
 
 app.use(router);
+app.use(pinia);
 app.mount("#app");
